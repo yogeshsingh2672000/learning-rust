3a91f0fad6f395bee585fd46b1c6d175df4e5fe5
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-10-19 15:17:43 +0530

    appropriate spacing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,17 +7,19 @@ fn main() {
 
     let mut guess: String = String::new();
     io::stdin().read_line(&mut guess).expect("failed to read line");
-
+    
     let secret_number = rand::thread_rng().gen_range(0, 101);
     println!("Secret number is: {}", secret_number);
-
+    
     let guess: u32 = guess.trim().parse().expect("Pleae type number");
-
     println!("Your guess is: {}", guess);
+    
+    
+    
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too Small!"),
-        Ordering::Equal => println!("Equal!"),
+        Ordering::Equal => println!("You Win!"),
         Ordering::Greater => println!("Too Greater!")
     }
 }
